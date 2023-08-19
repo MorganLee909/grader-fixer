291bda86f762f6ca30c24bd01880d3eec3b5a61d
Lee Morgan <me@leemorgan.io>
2023-08-19 06:42:54 -0400

    Fixed pagination problems on the Claimed by Me page.

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ Just a simple script to make the grader easier to use. There are some much neede
 1. When the assignment has been graded previously, the grade is displayed in "Comments", next to the timestamp for the specific grader comment.
 2. All comments are split up by line instead of displayed in one massive block of text, making it easy to read both comments from previous graders and from the students.
 3. Rubric has been added to the "Rubric" section (work in progress, style is very bad)
+4. Displaying of assignments on the "Claimed by Me" page has been fixed.
+    1. Problem with "Active" assignments being spread across multiple pages is fixed.
+    2. Pagination has been removed. Each category (Active, Past Due, Waiting for Review) is displayed in its own single scrollable list.
+    3. Speed improved. No more loading of assignments when switching category, it is automatic.
 
 ## BUGS
 If you run into any problems or bugs, please submit an issue on GitHub. https://github.com/MorganLee909/grader-fixer/issues
--- script.js
+++ script.js
@@ -13,6 +13,11 @@
     'use strict';
     let currentPage = window.location.href;
 
+    //CREATE CLONABLE ASSIGNMENTCARD
+    let clonableAssCard = document.createElement("div");
+    clonableAssCard.innerHTML = '<div style="padding: 10px; border: 1px solid rgb(222, 222, 223); border-radius: 5px;" class="item queue-item canvas"><div style="position: relative;" class="content"><a href="https://grading.bootcampspot.com/canvasSubmission/820697"><div style="font-size: 20px; font-weight: bold; color: black; width: 50%;" class="header">Module 21 Challenge</div></a><div style="display: flex; flex-direction: row-reverse; align-items: center;"><div style="margin-right: 10px; margin-top: -1.5em;"></div></div><div class="description"><div class="ui grid"><div class="six wide computer sixteen wide mobile six wide tablet column"><p>In Progress</p><p>2023-08-18T02:13:46.947976Z</p><p>2023-08-18T04:59:59Z</p><p>Full Stack Flex</p></div><div class="six wide computer sixteen wide mobile six wide tablet column"><p>Cotton, Gage</p><p>UTA-VIRT-FSF-PT-03-2023-U-LOLC-MWTH(A)</p></div></div></div><a style="bottom: 0px; right: 0px; position: absolute;" class="ui grey button" role="button" href="https://grading.bootcampspot.com/canvasSubmission/820697"><i class="right arrow icon"></i></a></div></div>';
+    clonableAssCard = clonableAssCard.firstChild;
+
     const applyStyle = ()=>{
         const style = `
             .gradeDisplay{
@@ -112,11 +117,122 @@
         rubricElem.appendChild(frame);
     }
 
+    const createAssCard = (data)=>{
+        let newCard = clonableAssCard.cloneNode(true);
+
+        let header = newCard.querySelector(".header");
+        header.textContent = data.assignmentName;
+        header.parentElement.href = `https://grading.bootcampspot.com/canvasSubmission/${data.id}`;
+
+        let descContainerLeft = newCard.querySelector(".description > .ui.grid").children[0];
+        descContainerLeft.children[0].textContent = data.status;
+        descContainerLeft.children[1].textContent = data.date;
+        descContainerLeft.children[2].textContent = data.assignmentDueDate;
+        descContainerLeft.children[3].textContent = data.program.name;
+
+        let descContainerRight = newCard.querySelector(".description > .ui.grid").children[1];
+        descContainerRight.children[0].textContent = data.studentName;
+        descContainerRight.children[1].textContent = data.course.name;
+
+        newCard.querySelector(".ui.grey.button").href = `https://grading.bootcampspot.com/canvasSubmission/${data.id}`;
+
+        return newCard;
+    }
+
+    const displayAssignments = (assignments)=>{
+        console.log(assignments);
+        let assCardContainer = document.querySelector(".ui.items");
+        let assCard = document.querySelector(".item.queue-item.canvas");
+
+        while(assCardContainer.children.length > 1){
+            assCardContainer.removeChild(assCardContainer.lastChild);
+        }
+        for(let i = 0; i < assignments.length; i++){
+            let newCard = createAssCard(assignments[i]);
+            assCardContainer.appendChild(newCard);
+        }
+    }
+
+    const replaceSecondaryMenu = (assignments)=>{
+        let menu = document.querySelector(".ui.secondary.menu");
+        for(let i = 0; i < menu.children.length; i++){
+            let newItem = menu.children[i].cloneNode(true);
+            menu.replaceChild(newItem, menu.children[i]);
+
+            let data = [];
+            switch(i){
+                case 0: data = assignments.ungraded; break;
+                case 1: data = assignments.pastDue; break;
+                case 2: data = assignments.waitingReview; break;
+            }
+
+            newItem.addEventListener("click", ()=>{
+                for(let i = 0; i < menu.children.length; i++){
+                    menu.children[i].classList.remove("active");
+                }
+                newItem.classList.add("active");
+                displayAssignments(data);
+            });
+        }
+    }
+
+    const getClaimed = ()=>{
+        fetch("https://grading.bootcampspot.com/api/centralgrading/v1/myClaimedSubmissions", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                offset: 0,
+                resultsPerPage: 500,
+                role: {
+                    id: 1,
+                    name: "Central Grader"
+                }
+            })
+        })
+            .then(r=>r.json())
+            .then((response)=>{
+                console.log(response);
+                let ungraded = [];
+                let waitingReview = [];
+                let pastDue = [];
+                for(let i = 0; i < response.data.length; i++){
+                    let status = response.data[i].status;
+                    if(status === "In Progress"){
+                        ungraded.push(response.data[i]);
+                    }else if(status === "Ready for Review" || status === "Flagged"){
+                        waitingReview.push(response.data[i]);
+                    }else{
+                        pastDue.push(response.data[i]);
+                    }
+                }
+
+                let assignments = {ungraded, waitingReview, pastDue};
+            console.log(assignments);
+                replaceSecondaryMenu(assignments);
+
+                if(ungraded.length > 0){
+                    let waiter = setInterval(()=>{
+                        let assCardContainer = document.querySelector(".ui.items");
+                        if(assCardContainer.children.length > 1){
+                            clearInterval(waiter);
+
+                            document.querySelector(".ui.pagination.menu").style.display = "none";
+                            displayAssignments(ungraded);
+                        }
+                    }, 100);
+                }
+            })
+            .catch((err)=>{
+                console.error(err);
+            });
+    }
+
     const submissionPage = ()=>{
         getAssignmentData();
 
         let loadedInterval = setInterval(()=>{
-            console.log("thing");
             if(!currentPage.includes("canvasSubmission")) clearInterval(loadedInterval);
             if(document.querySelectorAll(".ui.header").length > 0){
                 clearInterval(loadedInterval);
@@ -125,14 +241,21 @@
         }, 100);
     }
 
+    const claimedPage = ()=>{
+        getClaimed();
+    }
+
     // HANDLE PAGE CHANGES
     const handlePage = ()=>{
         if(currentPage.includes("canvasSubmission")) submissionPage();
+        if(currentPage.includes("claimed")) claimedPage();
     }
 
     setInterval(()=>{
         let newUrl = window.location.href;
         if(newUrl !== currentPage){
+            console.log(currentPage);
+            console.log(newUrl);
             currentPage = newUrl;
             handlePage();
         }
